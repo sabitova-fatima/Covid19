742c689b4c93c8801511a4d7083eef54d7b73239
Fatima <sabitova.fatima@gmail.com>
2020-11-22 19:59:42 +0300

    Update and rename draft.c to draft.r

--- draft.r
+++ draft.r
@@ -6,10 +6,6 @@ plot(c(1, 2, 3, 4, 5), c(5, 4, 3, 2, 1))
 cov <- read_csv("covid.csv")
 head(cov)
 
-glimpse(cov)
-
-cov %>% group_by(cov$countriesAndTerritories) %>% select(`Cumulative_number_for_14_days_of_COVID-19_cases_per_100000`) %>% summarise()
-
 rus <- cov %>% filter(countriesAndTerritories == 'Russia') %>% select(`Cumulative_number_for_14_days_of_COVID-19_cases_per_100000`, dateRep) %>% na.omit()
 
 mean(rus$`Cumulative_number_for_14_days_of_COVID-19_cases_per_100000`, na.rm = T)
