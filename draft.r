eed74f90e16a673d475891c87bd7da5a30d4f275
Fatima <sabitova.fatima@gmail.com>
2020-11-22 20:06:51 +0300

    Update draft.r

--- draft.r
+++ draft.r
@@ -6,9 +6,11 @@ plot(c(1, 2, 3, 4, 5), c(5, 4, 3, 2, 1))
 cov <- read_csv("covid.csv")
 head(cov)
 
-rus <- cov %>% filter(countriesAndTerritories == 'Russia') %>% select(`Cumulative_number_for_14_days_of_COVID-19_cases_per_100000`, dateRep) %>% na.omit()
+glimpse(cov)
+
+rus <- cov %>% filter(countriesAndTerritories == 'Russia') %>% select(cases, dateRep, popData2019) %>% na.omit()
 
 mean(rus$`Cumulative_number_for_14_days_of_COVID-19_cases_per_100000`, na.rm = T)
-# 57.06495
-  
-qplot(dmy(rus$dateRep), rus$`Cumulative_number_for_14_days_of_COVID-19_cases_per_100000`)
+
+qplot(dmy(rus$dateRep), (rus$cases))
+
